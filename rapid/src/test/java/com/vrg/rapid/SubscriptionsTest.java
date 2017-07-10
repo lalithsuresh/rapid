b0876fd59da81036fe7a05e2e6347b7411f6097e
lalithsuresh <suresh.lalith@gmail.com>
2017-07-10 13:13:28 -0700

    Wrap counters and callbacks in a class for subscriptions test code

--- rapid/src/test/java/com/vrg/rapid/SubscriptionsTest.java
+++ rapid/src/test/java/com/vrg/rapid/SubscriptionsTest.java
@@ -14,107 +14,98 @@ import static org.junit.Assert.assertEquals;
  * Tests whether subscription callbacks are invoked on cluster starts/joins
  */
 public class SubscriptionsTest {
-    @Test(timeout = 30000)
+    @Test(timeout = 5000)
     public void testSubscriptionOnJoin() throws IOException, InterruptedException {
         final HostAndPort seedHost = HostAndPort.fromParts("127.0.0.1", 1234);
         final HostAndPort joiner = HostAndPort.fromParts("127.0.0.1", 1235);
 
         // Initialize seed
-        final AtomicInteger seedCounter = new AtomicInteger(0);
-        final Consumer<List<NodeStatusChange>> consumerSeed = (List<NodeStatusChange> status)
-                                                               -> seedCounter.incrementAndGet();
+        final TestCallback seedCb = new TestCallback();
         final Cluster seedCluster = new Cluster.Builder(seedHost)
-                                               .addSubscription(ClusterEvents.VIEW_CHANGE, consumerSeed)
+                                               .addSubscription(ClusterEvents.VIEW_CHANGE, seedCb)
                                                .start();
 
         // Initialize joiner
-        final AtomicInteger joinerCounter = new AtomicInteger(0);
-        final Consumer<List<NodeStatusChange>> consumer = (List<NodeStatusChange> status)
-                                                           -> joinerCounter.incrementAndGet();
+        final TestCallback joinCb = new TestCallback();
         final Cluster nonSeed = new Cluster.Builder(joiner)
-                .addSubscription(ClusterEvents.VIEW_CHANGE, consumer)
+                .addSubscription(ClusterEvents.VIEW_CHANGE, joinCb)
                 .join(seedHost);
 
-        // Check if counters got incremented
-        assertEquals(2, seedCounter.get());
-        assertEquals(1, joinerCounter.get());
+        assertEquals(2, seedCb.numTimesCalled());
+        assertEquals(1, joinCb.numTimesCalled());
 
         seedCluster.shutdown();
         nonSeed.shutdown();
     }
 
-    @Test(timeout = 30000)
+    @Test(timeout = 5000)
     public void testMultipleSubscriptionsOnJoin() throws IOException, InterruptedException {
         final HostAndPort seedHost = HostAndPort.fromParts("127.0.0.1", 1234);
         final HostAndPort joiner = HostAndPort.fromParts("127.0.0.1", 1235);
 
         // Initialize seed
-        final AtomicInteger seedCounter1 = new AtomicInteger(0);
-        final Consumer<List<NodeStatusChange>> consumerSeed1 = (List<NodeStatusChange> status)
-                -> seedCounter1.incrementAndGet();
-        final AtomicInteger seedCounter2 = new AtomicInteger(0);
-        final Consumer<List<NodeStatusChange>> consumerSeed2 = (List<NodeStatusChange> status)
-                -> seedCounter2.incrementAndGet();
+        final TestCallback seedCb1 = new TestCallback();
+        final TestCallback seedCb2 = new TestCallback();
         final Cluster seedCluster = new Cluster.Builder(seedHost)
-                .addSubscription(ClusterEvents.VIEW_CHANGE, consumerSeed1)
-                .addSubscription(ClusterEvents.VIEW_CHANGE, consumerSeed2)
+                .addSubscription(ClusterEvents.VIEW_CHANGE, seedCb1)
+                .addSubscription(ClusterEvents.VIEW_CHANGE, seedCb2)
                 .start();
 
         // Initialize joiner
-        final AtomicInteger joinerCounter1 = new AtomicInteger(0);
-        final AtomicInteger joinerCounter2 = new AtomicInteger(0);
-        final Consumer<List<NodeStatusChange>> joinerConsumer1 = (List<NodeStatusChange> status)
-                -> joinerCounter1.incrementAndGet();
-        final Consumer<List<NodeStatusChange>> joinerConsumer2 = (List<NodeStatusChange> status)
-                -> joinerCounter2.incrementAndGet();
+        final TestCallback joinCb1 = new TestCallback();
+        final TestCallback joinCb2 = new TestCallback();
         final Cluster nonSeed = new Cluster.Builder(joiner)
-                .addSubscription(ClusterEvents.VIEW_CHANGE, joinerConsumer1)
-                .addSubscription(ClusterEvents.VIEW_CHANGE, joinerConsumer2)
+                .addSubscription(ClusterEvents.VIEW_CHANGE, joinCb1)
+                .addSubscription(ClusterEvents.VIEW_CHANGE, joinCb2)
                 .join(seedHost);
 
-        // Check if counters got incremented
-        assertEquals(2, seedCounter1.get());
-        assertEquals(2, seedCounter2.get());
-        assertEquals(1, joinerCounter1.get());
-        assertEquals(1, joinerCounter2.get());
+        assertEquals(2, seedCb1.numTimesCalled());
+        assertEquals(2, seedCb2.numTimesCalled());
+        assertEquals(1, joinCb1.numTimesCalled());
+        assertEquals(1, joinCb2.numTimesCalled());
 
         seedCluster.shutdown();
         nonSeed.shutdown();
     }
 
-    @Test(timeout = 30000)
+    @Test(timeout = 5000)
     public void testSubscriptionPostJoin() throws IOException, InterruptedException {
         final HostAndPort seedHost = HostAndPort.fromParts("127.0.0.1", 1234);
         final HostAndPort joiner = HostAndPort.fromParts("127.0.0.1", 1235);
 
         // Initialize seed
-        final AtomicInteger seedCounter = new AtomicInteger(0);
-        final Consumer<List<NodeStatusChange>> consumerSeed = (List<NodeStatusChange> status)
-                -> seedCounter.incrementAndGet();
+        final TestCallback seedCb1 = new TestCallback();
         final Cluster seedCluster = new Cluster.Builder(seedHost)
-                .addSubscription(ClusterEvents.VIEW_CHANGE, consumerSeed)
+                .addSubscription(ClusterEvents.VIEW_CHANGE, seedCb1)
                 .start();
 
-        final AtomicInteger seedCounter2 = new AtomicInteger(0);
-        final Consumer<List<NodeStatusChange>> consumerSeed2 = (List<NodeStatusChange> status)
-                -> seedCounter2.incrementAndGet();
-        seedCluster.registerSubscription(ClusterEvents.VIEW_CHANGE, consumerSeed2);
+        final TestCallback seedCb2 = new TestCallback();
+        seedCluster.registerSubscription(ClusterEvents.VIEW_CHANGE, seedCb2);
 
         // Initialize joiner
-        final AtomicInteger joinerCounter = new AtomicInteger(0);
-        final Consumer<List<NodeStatusChange>> consumer = (List<NodeStatusChange> status)
-                -> joinerCounter.incrementAndGet();
+        final TestCallback consumer1 = new TestCallback();
         final Cluster nonSeed = new Cluster.Builder(joiner)
-                .addSubscription(ClusterEvents.VIEW_CHANGE, consumer)
+                .addSubscription(ClusterEvents.VIEW_CHANGE, consumer1)
                 .join(seedHost);
 
-        // Check if counters got incremented
-        assertEquals(2, seedCounter.get());
-        assertEquals(1, seedCounter2.get());
-        assertEquals(1, joinerCounter.get());
+        assertEquals(2, seedCb1.numTimesCalled());
+        assertEquals(1, seedCb2.numTimesCalled());
+        assertEquals(1, consumer1.numTimesCalled());
 
         seedCluster.shutdown();
         nonSeed.shutdown();
     }
 
+    private static class TestCallback implements Consumer<List<NodeStatusChange>> {
+        private final AtomicInteger counter = new AtomicInteger(0);
+
+        @Override
+        public void accept(final List<NodeStatusChange> nodeStatusChanges) {
+            counter.incrementAndGet();
+        }
+
+        int numTimesCalled() {
+            return counter.get();
+        }
+    }
 }
